11c52c226ea42fe70c1ac0af4bed4ba9fd05ec07
Erwan <erwan.durand1@etu.univ-orleans.fr>
2018-03-14 13:46:41 +0100

    ajout des teams et mise en place des cadres

--- smite.js
+++ smite.js
@@ -1,6 +1,6 @@
 $(function() {
 teamList();
-
+afficherTeam("NRG Esports");
     function teamList(){
       $.ajax({
         url: "http://localhost:3000/equipe",
@@ -14,12 +14,26 @@ teamList();
               .append($('<li class="nav-item" data-toggle="tooltip" data-placement="right" >')
               .append($('<a class="nav-link" href="index.html">')
               .append($("<img src='"+equipe[i].logo+"' class='fa fa-fw fa-dashboard'>"))
-              .append($("<span class='nav-link-text'>").text(equipe[i].nom))))
+              .append($("<span class='nav-link-text'>").text("  "+equipe[i].nom))))
 
           }
         }
         });
 
 
+  }
+  function afficherTeam(nom){
+    document.getElementById('liste').innerHTML = "";
+        document.getElementById('logo-nom').innerHTML = "";
+    document.getElementById('info-joueur').innerHTML = "";
+    document.getElementById('palmares').innerHTML = "";
+    $.ajax({
+      url:"http://localhost:3000/equipe",
+      type: "GET",
+      dataType: "json",
+      success: function(equipe){
+        $('#logo-nom').append($("<img src='"+equipe[nom].logo+"'"))
+      }
+    });
   }
 });
