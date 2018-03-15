8f423b6e65846dc7065e97bdc04465c2e892a7cf
q.cognard1 <q.cognard@hotmail.fr>
2018-03-15 10:08:36 +0100

    Ajout de fonctionnalité permettant d'avoir les informations d'une team et de ses joueurs

--- smite.js
+++ smite.js
@@ -1,6 +1,6 @@
 $(function() {
 teamList();
-afficherTeam("NRG Esports");
+// afficherTeam("NRG Esports");
     function teamList(){
       $.ajax({
         url: "http://localhost:3000/equipe",
@@ -12,28 +12,96 @@ afficherTeam("NRG Esports");
               console.log(equipe[i]);
               $('#exampleAccordion')
               .append($('<li class="nav-item" data-toggle="tooltip" data-placement="right" >')
-              .append($('<a class="nav-link" href="index.html">')
+              .append($('<a class="nav-link">').on("click", equipe[i], details)
               .append($("<img src='"+equipe[i].logo+"' class='fa fa-fw fa-dashboard'>"))
-              .append($("<span class='nav-link-text'>").text("  "+equipe[i].nom))))
+              .append($("<span class='nav-link-text'>").text("  "+equipe[i].nom))));
+
 
           }
         }
         });
-
-
   }
-  function afficherTeam(nom){
-    document.getElementById('liste').innerHTML = "";
-        document.getElementById('logo-nom').innerHTML = "";
-    document.getElementById('info-joueur').innerHTML = "";
-    document.getElementById('palmares').innerHTML = "";
-    $.ajax({
-      url:"http://localhost:3000/equipe",
-      type: "GET",
-      dataType: "json",
-      success: function(equipe){
-        $('#logo-nom').append($("<img src='"+equipe[nom].logo+"'"))
+
+  function details(event){
+      $("#listejoueurs").empty();
+      $("#palmares").empty();
+      formEquipe();
+      fillFormEquipe(event.data);
+      console.log("event");
       }
-    });
-  }
+
+    function detailsJoueur(event){
+        $("#infojoueur").empty();
+        formJoueur();
+        fillFormJoueur(event.data);
+        }
+
+    function formJoueur(){
+        $("#infojoueur")
+          .append($("<img id='logojoueur'>"))
+          .append($('<h1 id="nomjoueure"></h1>'))
+          .append($("<p id='prenom'>"))
+          .append($("<p id='nom'>"))
+          .append($("<p id='anniversaire'>"))
+          .append($("<p id='role'>"))
+          .append($("<p id='originejoueur'>"));
+
+
+        }
+
+  function formEquipe(){
+      $("#listejoueurs")
+        .append($("<section id='logo-nom'>")
+        .append($("<img id='logoteam'>"))
+        .append($('<h1 id="nomteam"></h1>')))
+        .append($("<section id='secliste'>")
+        .append($("<ul id='liste'>")));
+      $("#palmares")
+        .append($("<ul id='listepalma'>"));
+      }
+
+    function fillFormJoueur(t){
+      $("#logojoueur").attr('src',t.image);
+      $("#nomjoueure").text(t.pseudo);
+      $("#prenom").text(t.prenom);
+      $("#nom").text(t.nom);
+      $("#anniversaire").text(t.anniversaire);
+      $("#role").text(t.role);
+      $("#originejoueur").text(t.originejoueur);
+
+
+    }
+
+      function fillFormEquipe(t){
+        $("#nomteam").text(t.nom);
+        $("#logoteam").attr('src',t.logo);
+        for (var i=0;i<t.joueurs.length;i++){
+          $("#liste")
+          .append($("<li> <a id='"+t.joueurs[i].id+"'>"+t.joueurs[i].pseudo+"</a></li>").on("click", t.joueurs[i], detailsJoueur));
+        }
+        for (var i=0;i<t.palmares.length;i++){
+          $("#listepalma")
+          .append($("<li>"+t.palmares[i].date+" | "+t.palmares[i].place+" | "+t.palmares[i].nomEvent+"</li>"));
+        }
+
+      }
+
+
+  // function afficherTeam(nom){
+  //   document.getElementById('liste').innerHTML = "";
+  //       document.getElementById('logo-nom').innerHTML = "";
+  //   document.getElementById('info-joueur').innerHTML = "";
+  //   document.getElementById('palmares').innerHTML = "";
+  //   $.ajax({
+  //     url:"http://localhost:3000/equipe",
+  //     type: "GET",
+  //     dataType: "json",
+  //     success: function(equipe){
+  //       $('#logo-nom').append($("<img src='"+equipe[nom].logo+"'"))
+  //     }
+  //   });
+  // }
+
+
+
 });
